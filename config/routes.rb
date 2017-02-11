ad327c65c5e1894c99183b1ca1a8260d51af5cb0
James Harris <computersherpa@gmail.com>
2017-02-10 18:03:51 -0600

    Create Students Ruby controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :advisors
   get 'angular_app/index'
 
+  scope :api do
+    scope :v1 do
+      resources :students
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'angular_app#index'
