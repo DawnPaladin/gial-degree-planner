130b18aaa17e14816ce96740951ae7e04d8efb5b
luke-schleicher <luke.j.schleicher@gmail.com>
2017-02-10 21:03:47 -0600

    setting up advisor creation with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :advisors
-  get 'angular_app/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :advisors, controllers: { sessions: 'advisors/sessions', registrations: 'advisors/registrations'}
 
   root 'angular_app#index'
   get 'angular_app/index'
