a44192ed7a44c3853b89ccb47edfd744ba7ad29c
James Harris <computersherpa@gmail.com>
2017-02-10 18:04:37 -0600

    Load and display basic student data

--- app/assets/javascripts/controllers/StudentsIndexCtrl.js
+++ app/assets/javascripts/controllers/StudentsIndexCtrl.js
@@ -1,3 +1,16 @@
-planner.controller('StudentsIndexCtrl', ['$scope', function($scope) {
-  $scope.hello = "Hello World";
+planner.controller('StudentsIndexCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
+
+  // var students = Restangular.all('students');
+  // students.getList().then(function(students) {
+  //   console.log(students);
+  //   $scope.students = students;
+  // });
+
+  $scope.students = Restangular.all('students').getList().$object;
+
+  Restangular.all('students').getList().then(function(result) {
+    console.log(result);
+    // $scope.students = result;
+  });
+
 }]);
