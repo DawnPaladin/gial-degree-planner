f391949618ae175dee3f9b821a28c11a62b74dd8
luke-schleicher <luke.j.schleicher@gmail.com>
2017-02-10 23:30:59 -0600

    fixed devise routing and flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
+
+  private
+
+    def after_sign_out_path_for(resource_or_scope)
+      new_advisor_session_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :advisors, controllers: { sessions: 'advisors/sessions', registrations: 'advisors/registrations'}
 
+  devise_scope :advisor do
+    get 'advisors/sign_out', to: 'advisors/sessions#destroy'
+  end
+
   root 'angular_app#index'
+
   get 'angular_app/index'
+
 end
