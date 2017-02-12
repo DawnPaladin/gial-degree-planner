d6d3cf47d124c8b16c6c1afa7675d279ba58f67f
James Harris <computersherpa@gmail.com>
2017-02-12 17:21:32 -0600

    Sort by property, and reverse sort on second click

--- app/assets/javascripts/controllers/StudentsIndexCtrl.js
+++ app/assets/javascripts/controllers/StudentsIndexCtrl.js
@@ -8,6 +8,15 @@ planner.controller('StudentsIndexCtrl', ['$scope', 'Restangular', function($scop
 
   $scope.students = Restangular.all('students').getList().$object;
   $scope.advisors = Restangular.all('advisors').getList().$object;
+  $scope.property = "last_name";
+
+  $scope.alternate = function(property) {
+    if ($scope.property == property) {
+      $scope.property = "-" + property;
+    } else {
+      $scope.property = property;
+    }
+  };
 
   Restangular.all('students').getList().then(function(result) {
     console.log(result);
