8dd5ade482cb74ab493a2e95bb113dd8f4fa125a
Alexa <alexa.anderson@outlook.com>
2017-02-12 20:33:36 -0600

    Begin model tests

--- spec/factories/advisors.rb
+++ spec/factories/advisors.rb
@@ -1,5 +1,16 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :advisor do
     
+    first_name 'Advisor'
+    last_name 'Smith'
+    sequence(:email) { |n| "advisor#{n}@gial.edu"}
+    password 'password'
+    is_admin false
+
+    trait(:admin) do
+      is_admin true
+    end
   end
 end
