eacb18e0eb66e0eeac91c89db413cbb687f4354d
Alexa <alexa.anderson@outlook.com>
2017-02-14 09:48:46 -0600

    Refactor initial ips load. All static info comes over.

--- app/assets/javascripts/controllers/IPSHeaderCtrl.js
+++ app/assets/javascripts/controllers/IPSHeaderCtrl.js
@@ -1,11 +1,15 @@
-planner.controller('IPSHeaderCtrl', ['$scope', 'student', 'planService', 'concentrationService',
-  function($scope, student, planService, concentrationService) {
+planner.controller('IPSHeaderCtrl', ['$scope', 'student', 'planService',
+  function($scope, student, planService) {
   
   $scope.student = student;
 
-  // TODO get from plan instead of student
-  $scope.concentrations = student.plan.degree.concentrations;
+  // All static information comes over
+  // on initial load
+  // Student + Degree + List of Concentrations
+  $scope.concentrations = student.degree.concentrations;
+
   $scope.planInfo = planService.getPlanInfo();
+  $scope.plan = $scope.planInfo.plan;
 
   $scope.terms = ['SPRING', 'SUMMER', 'FALL'];
 
--- app/views/students/show.json.jbuilder
+++ app/views/students/show.json.jbuilder
@@ -1,11 +1,6 @@
 json.extract! @student, *@student.attributes.keys
-json.plan do
-  json.extract! @plan, *@plan.attributes.keys
-  json.term = @term
-  json.courses @plan.courses
-  json.degree do
-    json.name @degree.name
-    json.description @degree.description
-    json.concentrations @degree.concentrations
-  end
+json.degree do
+  json.name @degree.name
+  json.description @degree.description
+  json.concentrations @degree.concentrations
 end
