15ccaae38e4a21f56c05b0f20713a984e8bc783f
luke-schleicher <luke.j.schleicher@gmail.com>
2017-02-14 17:12:22 -0600

    Student Schedule: displaying schedule

--- app/assets/javascripts/controllers/IPSScheduleCtrl.js
+++ app/assets/javascripts/controllers/IPSScheduleCtrl.js
@@ -4,4 +4,17 @@ planner.controller('IPSScheduleCtrl', ['$scope', '$rootScope', 'planService', fu
 
   $scope.planInfo = planService.getPlanInfo();
 
+  $scope.possibleYears = $scope.planInfo.possibleYears;
+
+  $scope.years = [$scope.possibleYears[0]];
+
+  var nextYear = 1;
+
+  $scope.addYear = function() {
+    if ($scope.possibleYears[nextYear]) {
+      $scope.years.push($scope.possibleYears[nextYear]);
+      nextYear++;
+    }
+  };
+
 }]);
