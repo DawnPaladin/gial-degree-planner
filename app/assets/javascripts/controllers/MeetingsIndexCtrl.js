86a5b73ac612da7ed4a1be4d425e7187d5d70279
James Harris <computersherpa@gmail.com>
2017-02-14 21:25:44 -0600

    X and Y axes for table

--- app/assets/javascripts/controllers/MeetingsIndexCtrl.js
+++ app/assets/javascripts/controllers/MeetingsIndexCtrl.js
@@ -1,18 +1,21 @@
 planner.controller('MeetingsIndexCtrl', ['$scope', 'courses',
   function($scope, courses) {
     $scope.courses = courses;
-    // $scope.terms = [{
-    //   name: "Spring",
-    //   sessions: ["S1", "S2", "S3", "S4"]
-    // }, {
-    //   name: "Summer",
-    //   sessions: [""]
-    // }, {
-    //   name: "Fall",
-    //   sessions: [1,2,3,4]
-    // }];
+    $scope.terms = [{
+      name: "Spring",
+      width: 4,
+      sessions: ["S1", "S2", "S3", "S4"]
+    }, {
+      name: "Summer",
+      width: 1,
+      sessions: [""]
+    }, {
+      name: "Fall",
+      width: 4,
+      sessions: [1,2,3,4]
+    }];
     // $scope.terms = ["2016", "Spring", "S1", "S2", "S3", "S4", "Summer", "Fall", "S1", "S2", "S3", "S4"]
-    $scope.terms = ["Spring", "Summer", "Fall"];
+    // $scope.terms = ["Spring", "Summer", "Fall"];
     $scope.sessions = ["S1", "S2", "S3", "S4", "Summer", "S1", "S2", "S3", "S4"];
 
     $scope.meetings = [];
