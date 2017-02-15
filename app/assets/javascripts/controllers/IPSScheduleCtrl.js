1cc9def1c5315a16abe49d45c17c52eb7c054351
luke-schleicher <luke.j.schleicher@gmail.com>
2017-02-15 10:30:42 -0600

    unscheduled courses fix to top of screen

--- app/assets/javascripts/controllers/IPSScheduleCtrl.js
+++ app/assets/javascripts/controllers/IPSScheduleCtrl.js
@@ -1,4 +1,4 @@
-planner.controller('IPSScheduleCtrl', ['$scope', '$rootScope', 'planService', function($scope, $rootScope, planService) {
+planner.controller('IPSScheduleCtrl', ['$scope', '$rootScope', 'planService', '$window', function($scope, $rootScope, planService, $window) {
 
   $rootScope.$broadcast('toggle-concentration');
 
@@ -21,6 +21,42 @@ planner.controller('IPSScheduleCtrl', ['$scope', '$rootScope', 'planService', fu
   $scope.handleDrop = function() {
     console.log('dropped');
   };
+
+  var stickyContainer = document.getElementById('sticky-container');
+  var stickyContainerLocation = stickyContainer.getBoundingClientRect().top;
+  var stickyContainerWidth = stickyContainer.offsetWidth;
+  var page = document.getElementById('page');
+
+  var stuckCourses = function() {
+    stickyContainer.setAttribute("style", 'position: fixed; top: 0; left: 0; background: rgba(240, 240, 240, 0.8);');
+  };
+
+  var unstuckCourses = function() {
+    stickyContainer.setAttribute("style", 'position: static;')
+  }
+
+  var stickyCourses = function() {
+    console.log(page.scrollTop);
+    console.log(stickyContainerLocation);
+    if (page.scrollTop > stickyContainerLocation) {
+      console.log('true');
+      stuckCourses();
+    } else {
+      unstuckCourses();
+    }
+  };
+
+  $window.addEventListener('load', function() {
+    $window.off();
+    $window.addEventListener('scroll', function(e) {
+      stickyCourses();
+    });
+  });
+
+  $window.addEventListener('scroll', function(e) {
+    console.log('scroll');
+    stickyCourses();
+  });
 
 
 
