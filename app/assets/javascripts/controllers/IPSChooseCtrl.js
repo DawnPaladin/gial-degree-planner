df6fed0703904b0b9abc32ae098ceb22a2fb6574
Alexa <alexa.anderson@outlook.com>
2017-02-15 23:20:44 -0600

    fFinish and comment IPS choose

--- app/assets/javascripts/controllers/IPSChooseCtrl.js
+++ app/assets/javascripts/controllers/IPSChooseCtrl.js
@@ -1,48 +1,25 @@
-planner.controller('IPSChooseCtrl', ['$scope', '$state', '$rootScope', 'student', 'plan', 'planService', 'concentrationService', '_', function($scope, $state, $rootScope, student, plan, planService, concentrationService, _) {
+planner.controller('IPSChooseCtrl', ['$scope', '$state', '$rootScope', 'student', 'plan', 'planService', function($scope, $state, $rootScope, student, plan, planService) {
   
   $scope.student = student;
   $scope.planInfo = plan;
-  $scope.courses = $scope.planInfo.plan.coursesById;
-  // $scope.concentration =
-  //   concentrationService
-  //     .getConcentration($scope.planInfo.plan.concentration_id);
-
-  $scope.intendedCourses = $scope.planInfo.plan.intended_courses;
-  $scope.requiredCourses = $scope.planInfo.plan.required_courses;
-  $scope.completedCourses = $scope.planInfo.plan.completed_courses;
-  $scope.coursesByCategory = $scope.planInfo.plan.available_courses;
-
-  // If the concentration of the plan changes,
-  // change the scope variable
-  $scope.$watch('planInfo.plan.concentration_id',
-    function(newConcentration, prevConcentration, scope) {
-      scope.concentration = 
-        concentrationService.getConcentration(newConcentration);
-  }, true);
-
-  // For all plan updates except for updates to latest_registered and registration_date
-  $scope.updatePlan = function() {
-    planService.update($scope.planInfo.plan);
-  };
 
   // CHECKBOX CALLBACKS:
   // Passed into course-row directives
   $scope.addOrRemoveIntended = function(course) {
     planService.addOrRemoveIntended(course);
+    $rootScope.$broadcast('planChanged', course);
   };
 
   $scope.toggleCompleted = function(course) {
     course.completed = !course.completed;
-    console.log('completed', course.completed)
     course.intended = !course.intended;
-    console.log('intended', course.intended)
     planService.addOrRemoveCompleted(course);
-    // planService.addOrRemoveIntended(course);
-  };
-
-  $scope.logCourse = function(course) {
-    console.log('here')
-    console.log(course);
+    $rootScope.$broadcast('planChanged', course);
   };
 
 }]);
+
+// Two days,
+// 3 unused services,
+// and ~10000 LOC later,
+// this is what remains.
--- app/assets/javascripts/services/planService.js
+++ app/assets/javascripts/services/planService.js
@@ -2,10 +2,18 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
 
   var _planInfo = {};
   
+  // This is called once from HeaderCtrl
+  // Would be better to use the IPS resolve,
+  // but changing it to do so,
+  // references were lost
+  // TODO: investigate
   var getPlanInfo = function() {
     return _planInfo;
   };
 
+  // This is called in the IPS resolve
+  // should probably be renamed to getplaninfo,
+  // but let's merge up first
   var getPlan = function(student_id) {
     return Restangular.one('students', student_id)
       .customGET('plan')
@@ -23,13 +31,17 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
       });
   };
 
+  // Makes data from backend useful for front
+  // TODO: refactor into separate functions
   var _extractCourses = function(plan) {
     var required = plan.required_courses;
     var intended = plan.intended_courses;
     var completed = plan.completed_courses;
     plan.coursesById = {};
     
-
+    // available_courses are preset if
+    // a concentration has been set
+    // if it exists, populate the all-knowing coursesById
     if (plan.available_courses) {
       plan.available_courses.forEach(function(category) {
         category.courses.forEach(function(course) {
@@ -39,7 +51,10 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
       });
     }
     
-
+    // Go through coursesById and set the correct ones
+    // to required
+    // If a course by the same name is
+    // not already there, make one
     required.forEach(function(course) {
       if (plan.coursesById[course.id]) {
         plan.coursesById[course.id].required = true;
@@ -51,32 +66,34 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
         plan.coursesById[savedCourse.id] = savedCourse;
       }
     });
-
+    
+    // same for intended courses
     intended.forEach(function(course) {
       if (plan.coursesById[course.id]) {
         plan.coursesById[course.id].intended = true;
       } else {
-        // var savedCourse = angular.copy(course, {});
         course.intended = true;
         plan.coursesById[course.id] = course;
       }
     });
 
+    // same for completed courses
     completed.forEach(function(course) {
       if (plan.coursesById[course.id]) {
         plan.coursesById[course.id].completed = true;
         plan.coursesById[course.id].intended = false;
-        // possibly set intended to false, should not be the case, but here we are
       } else {
         var savedCourse = angular.copy(course, {});
         savedCourse.completed = true;
         plan.coursesById[savedCourse.id] = savedCourse;
       }
     });
-
-
   };
 
+
+  // This sets the course grouping that are used on screen
+  // Doing this like so ensures the courses in coursesById
+  // and the courses in these groupings are the same references
   var _initializeCourses = function(plan) {
     plan.intended_courses = _.filter(_.values(plan.coursesById), function(course) {
       return course.intended === true;
@@ -90,49 +107,62 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
   };
 
 
-
+  // is update function
+  // refactor to pass in registration info
+  // to avoid double updates
+  // eg var update = function(plan, registrationValue)
   var update = function(plan) {
     return Restangular.one('students', plan.student_id)
       .customPUT(plan, 'plan')
       .then(function(plan) {
-        console.log('here')
-        plan.completed_id = null;
-        plan.intended_id = null;
-        return plan;
+        _extractCourses(plan);
+        _initializeCourses(plan);
+        angular.copy(plan, _planInfo.plan);
+
+        return _planInfo;
+
     }, function(response) {
       console.error(response);
     });
   };
 
+
+  // used in callbacks
   var addOrRemoveIntended = function(course) {
     if (course.intended) {
       _addToIntended(course);
     } else {
       _removeFromIntended(course);
     }
-  
+
+    // rails controller configured to take intended_id
+    // and add or remove association conditionally
     _planInfo.plan.intended_id = course.id;
     update(_planInfo.plan);
   };
 
   var addOrRemoveCompleted = function(course) {
-
     if (course.completed) {
       _addToCompleted(course);
     } else {
       _removeFromCompleted(course);
     }
-
+    
+    // rails controller configured to take completed_id
+    // and add or remove association conditionally
     _planInfo.plan.completed_id = course.id;
 
     // Do this because intended must be toggled
-    // But this way only one update call
+    // every time completed is
+    // this way there's only one update call
     addOrRemoveIntended(course);
   };
 
 
-  // TODO if these remain unchanged
-  // refactor into two functions
+  // NOTE: these are purely front-end functions.
+  // I tried to get this to work as Restangular collections..
+  // but here we are
+  // completed/intended_courses are rendered visually
   var _addToIntended = function(course) {
     _planInfo.plan.intended_courses.push(course);
   };
@@ -145,20 +175,16 @@ planner.factory('planService', ['Restangular', 'helpers', '_', function(Restangu
     for (var i = 0; i < _planInfo.plan.intended_courses.length; i++) {
       if (_planInfo.plan.intended_courses[i].id === course.id) {
         _planInfo.plan.intended_courses.splice(i, 1);
-        return true;
       }
     }
-    return false;
   };
 
   var _removeFromCompleted = function(course) {
     for (var i = 0; i < _planInfo.plan.completed_courses.length; i++) {
       if (_planInfo.plan.completed_courses[i].id === course.id) {
         _planInfo.plan.completed_courses.splice(i, 1);
-        return true;
       }
     }
-    return false;
   };
 
 
