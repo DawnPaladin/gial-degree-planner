0274cd2b41fa1cea6616fe50e6e2380e6850c90e
James Harris <computersherpa@gmail.com>
2017-02-17 15:11:50 -0600

    Multi-year table columns

--- app/assets/javascripts/controllers/MeetingsIndexCtrl.js
+++ app/assets/javascripts/controllers/MeetingsIndexCtrl.js
@@ -1,40 +1,31 @@
 planner.controller('MeetingsIndexCtrl', ['$scope', 'courses',
   function($scope, courses) {
     $scope.courses = courses;
-    $scope.terms = [{
-      name: "Spring",
-      width: 4,
-    }, {
-      name: "Summer",
-      width: 1,
-    }, {
-      name: "Fall",
-      width: 4,
-    }];
-    $scope.sessions = ["S1", "S2", "S3", "S4", "Summer", "S1", "S2", "S3", "S4"];
+    var years = ["2017", "2018", "2019", "2020"];
+    var terms = ["Spring", "Summer", "Fall"];
+    $scope.termHeader = [];
+    years.forEach(function(year) {
+      $scope.termHeader.push(year);
+      $scope.termHeader.push.apply($scope.termHeader, terms);
+    });
 
     var meetings = [];
     courses.forEach(function(course) {
       meetings.push.apply(meetings, course.meetings);
-      course.attendance = {
-        spring: ["","","",""],
-        summer: [""],
-        fall: ["","","",""],
-        any: []
+      course.attendance = [];
+      var yearAttendance = {
+        spring: "",
+        summer: "",
+        fall: "",
+        any: ""
       };
       course.meetings.forEach(function(meeting) {
         var term = meeting.term.toLowerCase();
-        var sessionIndex = meeting.session - 1;
-        if (sessionIndex < 0) { return; }
-        course.attendance[term][sessionIndex] = meeting.enrollments.length;
+        yearAttendance[term] = meeting.enrollments.length;
       });
-    });
-
-    $scope.years = [];
-    meetings.forEach(function(meeting) {
-      if ($scope.years.indexOf(meeting.year) === -1) {
-        $scope.years.push(meeting.year);
-      }
+      years.forEach(function() {
+        course.attendance.push("", yearAttendance.spring, yearAttendance.summer, yearAttendance.fall);
+      });
     });
 
   }
