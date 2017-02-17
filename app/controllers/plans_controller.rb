ef9fbe68d55d0c88d11c8a511f77299e7bc573cb
Alexa <alexa.anderson@outlook.com>
2017-02-16 21:38:48 -0600

    change to update attrs

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -9,8 +9,9 @@ class PlansController < ApplicationController
     @plan = Plan.find(params[:plan][:id])
     completed_course = Course.find_by_id(params[:completed_id])
     intended_course = Course.find_by_id(params[:intended_id])
+    binding.pry
     @plan.add_or_remove_courses(completed: completed_course, intended: intended_course)
-    if @plan.update(plan_params)
+    if @plan.update_attributes(plan_params)
       render :show, status: 200
     end
   end
