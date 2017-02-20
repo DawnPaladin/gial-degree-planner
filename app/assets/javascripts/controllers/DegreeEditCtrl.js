86c1e2cd6592194bbda73e52cc70214003904935
James Harris <computersherpa@gmail.com>
2017-02-20 12:45:49 -0600

    Update degree name on server

--- app/assets/javascripts/controllers/DegreeEditCtrl.js
+++ app/assets/javascripts/controllers/DegreeEditCtrl.js
@@ -1,5 +1,9 @@
 planner.controller('DegreeEditCtrl', ['$scope', 'degree',
   function($scope, degree) {
     $scope.degree = degree;
+
+    $scope.saveDegree = function() {
+      degree.put();
+    };
   }
-])
+]);
--- app/controllers/degrees_controller.rb
+++ app/controllers/degrees_controller.rb
@@ -8,4 +8,18 @@ class DegreesController < ApplicationController
     @degree = Degree.find_by_id(params[:id])
     render json: @degree.to_json
   end
+
+  def update
+    @degree = Degree.find_by_id(params[:id])
+    if @degree.update(degree_params)
+      render json: @degree.to_json
+    else
+      render json: @degree.errors.full_messages.to_json
+    end
+  end
+
+  private
+    def degree_params
+      params.require(:degree).permit(:name, :description)
+    end
 end
