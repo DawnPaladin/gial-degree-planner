6f661cd280e80829240a53ff3c5a945fab732588
Alexa Anderson <PopularDemand@users.noreply.github.com>
2017-02-21 10:03:46 -0600

    Refactor

--- app/assets/javascripts/directives/categorySection.js
+++ app/assets/javascripts/directives/categorySection.js
@@ -67,18 +67,9 @@ planner.directive('categorySection', ['Restangular', '$timeout', 'courseService'
       };
 
       scope.hideClassInput = function(event) {
-        console.log(event.relatedTarget)
         if (event.relatedTarget === null)
           scope.addingClass = false;
       };
-
-      // scope.setCourse = function(course) {
-      //   course = angular.copy(course, {});
-      //   course.category_id = scope.category.id;
-      //   scope.category.courses.push(course);
-      // };
-
-
     }
   };
 }]);
