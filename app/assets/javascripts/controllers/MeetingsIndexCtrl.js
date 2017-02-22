ba79c1b880ad4cd2f11b57a334c9bffa12cf98d6
James Harris <computersherpa@gmail.com>
2017-02-22 12:12:46 -0600

    Store attendance for each year

--- app/assets/javascripts/controllers/MeetingsIndexCtrl.js
+++ app/assets/javascripts/controllers/MeetingsIndexCtrl.js
@@ -10,23 +10,24 @@ planner.controller('MeetingsIndexCtrl', ['$scope', 'courses', 'meetingService',
     });
 
     var meetings = [];
-    courses.forEach(function(course) {
+    courses.forEach(function getCourseAttendance(course) {
       meetings.push.apply(meetings, course.meetings);
       course.attendance = [];
-      var yearAttendance = {
-        spring: "",
-        summer: "",
-        fall: "",
-        any: ""
-      };
-      course.meetings.forEach(function(meeting) {
-        var term = meeting.term.toLowerCase();
-        yearAttendance[term] = {
-          count: meeting.enrollments.length,
-          meeting_id: meeting.id
+      years.forEach(function(year) {
+        var yearAttendance = {
+          spring: {},
+          summer: {},
+          fall: {},
+          any: {},
         };
-      });
-      years.forEach(function() {
+        course.meetings.forEach(function storeYearAttendance(meeting) {
+          if (meeting.year !== Number(year)) { return; }
+          var term = meeting.term.toLowerCase();
+          yearAttendance[term] = {
+            count: meeting.enrollments.length,
+            meeting_id: meeting.id,
+          }
+        });
         course.attendance.push("", yearAttendance.spring, yearAttendance.summer, yearAttendance.fall);
       });
     });
