525f564a85e2377686f5873e0e9bf4f8980c5525
Alexa <alexa.anderson@outlook.com>
2017-02-24 01:19:59 -0600

    login styles, teacher titleize, order course rows;

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -7,4 +7,11 @@ class Teacher < ApplicationRecord
   has_many :meetings_teachers, dependent: :destroy
   has_many :meetings, through: :meetings_teachers
 
+  before_save :titleize_name
+
+  def titleize_name
+    self.first_name = self.first_name.titleize if self.first_name
+    self.last_name = self.last_name.titleize if self.last_name
+  end
+
 end
