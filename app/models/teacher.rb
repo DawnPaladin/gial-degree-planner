21b4455f03a2cd98b90d14ad90c36fd9313ef7ee
James Harris <computersherpa@gmail.com>
2017-02-24 09:52:14 -0600

    Remove titleize

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -7,11 +7,4 @@ class Teacher < ApplicationRecord
   has_many :meetings_teachers, dependent: :destroy
   has_many :meetings, through: :meetings_teachers
 
-  before_save :titleize_name
-
-  def titleize_name
-    self.first_name = self.first_name.titleize if self.first_name
-    self.last_name = self.last_name.titleize if self.last_name
-  end
-
 end
