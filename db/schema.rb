34925a759b184e029b8cd97178d1dd639266e8d3
James Harris <computersherpa@gmail.com>
2019-12-27 16:02:30 -0600

    Rerun database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -198,15 +198,6 @@ ActiveRecord::Schema.define(version: 20170402225737) do
     t.index ["concentration_id"], name: "index_non_thesis_tracks_on_concentration_id", using: :btree
   end
 
-  create_table "plan_years", force: :cascade do |t|
-    t.integer  "plan_id"
-    t.integer  "year_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["plan_id"], name: "index_plan_years_on_plan_id", using: :btree
-    t.index ["year_id"], name: "index_plan_years_on_year_id", using: :btree
-  end
-
   create_table "plans", force: :cascade do |t|
     t.integer  "student_id",                        null: false
     t.integer  "concentration_id"
@@ -217,13 +208,6 @@ ActiveRecord::Schema.define(version: 20170402225737) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.integer  "degree_id"
-  end
-
-  create_table "plans_years", force: :cascade do |t|
-    t.integer "plan_id"
-    t.integer "year_id"
-    t.index ["plan_id"], name: "index_plans_years_on_plan_id", using: :btree
-    t.index ["year_id"], name: "index_plans_years_on_year_id", using: :btree
   end
 
   create_table "sessions", force: :cascade do |t|
