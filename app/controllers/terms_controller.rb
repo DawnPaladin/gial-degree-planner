86847eb1c4b78452bd39986306ae05055672ef8d
James Harris <computersherpa@gmail.com>
2020-03-19 17:32:50 -0500

    "include" is for associations, "only" is for properties

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,7 +2,7 @@ class TermsController < ApplicationController
 
   def index
     @terms = Term.includes(:sessions).all
-    render json: @terms.to_json(include: :sessions, :abbreviation)
+    render json: @terms.to_json(include: :sessions, only: :abbreviation)
   end
 
 end
