cc88410024153ffe5ada825ccfa259304e20e683
James Harris <dawn@glim.local>
2020-08-27 23:06:36 -0500

    Make summary match Arts & Scripture Engagement form

--- app/assets/javascripts/controllers/IPSSummaryCtrl.js
+++ app/assets/javascripts/controllers/IPSSummaryCtrl.js
@@ -30,15 +30,23 @@ planner.controller('IPSSummaryCtrl', ['$scope', 'plan', 'student', '$rootScope',
     Restangular.one('concentrations', $scope.planInfo.plan.concentration_id).get()
     .then(function(concentrationInfo) {
       var category = concentrationInfo.categories.find(function(category) {
-        return category.name == "Concentration-specific courses"
+        return category.name == "Concentration-specific courses";
       });
       if (category) angular.copy(category.courses, $scope.concentrationSpecificCourses);
+    });
+  }
+
+  // The "Arts & Scripture Engagement" concentration needs to show the courses available in the category named "Choose One of the Following"
+  function getCoursesForChooseOne() {
+    $scope.chooseOne = $scope.planInfo.plan.available_courses.find(function(category) {
+      return category.name == "Choose one of the following";
     });
   }
 
   $scope.totalCoreCredits = countCredits(plan.plan.required_courses);
   $scope.$watch('planInfo.plan.concentration', getCategoryHourTotals);
   $scope.$watch('planInfo.plan.concentration', getConcentrationSpecificCourses);
+  $scope.$watch('planInfo.plan.concentration', getCoursesForChooseOne);
 
 
 }]);
