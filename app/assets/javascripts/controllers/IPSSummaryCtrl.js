59030b41b723f62a62c8563b66efdb3c1a291540
James Harris <computersherpa@gmail.com>
2020-09-09 16:06:55 -0500

    Align dotted lines
    Unify the functions in the controller that fetch concentration info
    Switch from floats to flexbox for dotted lines

--- app/assets/javascripts/controllers/IPSSummaryCtrl.js
+++ app/assets/javascripts/controllers/IPSSummaryCtrl.js
@@ -18,35 +18,61 @@ planner.controller('IPSSummaryCtrl', ['$scope', 'plan', 'student', '$rootScope',
     return sum;
   }
 
-  function getCategoryHourTotals() {
+  function getConcentrationInfo() {
     Restangular.one('concentrations', $scope.planInfo.plan.concentration_id).get()
-    .then(function(concentrationInfo) {
-      var requiredHoursByCategory = _.pluck(concentrationInfo.categories, 'required_units');
-      angular.copy(requiredHoursByCategory, $scope.requiredHoursByCategory)
-    });
+    .then(getCategoryHourTotals)
+    .then(getConcentrationSpecificCourses)
+    .then(getCoursesForChooseOne)
+    .then(window.setTimeout(alignDottedLines, 100))
   }
 
-  function getConcentrationSpecificCourses() {
-    Restangular.one('concentrations', $scope.planInfo.plan.concentration_id).get()
-    .then(function(concentrationInfo) {
-      var category = concentrationInfo.categories.find(function(category) {
-        return category.name == "Concentration-specific courses";
-      });
-      if (category) angular.copy(category.courses, $scope.concentrationSpecificCourses);
+  function getCategoryHourTotals(concentrationInfo) {
+    var requiredHoursByCategory = _.pluck(concentrationInfo.categories, 'required_units');
+    angular.copy(requiredHoursByCategory, $scope.requiredHoursByCategory);
+    return concentrationInfo;
+  }
+
+  function getConcentrationSpecificCourses(concentrationInfo) {
+    var category = concentrationInfo.categories.find(function(category) {
+      return category.name == "Concentration-specific courses";
     });
+    if (category) $scope.concentrationSpecificCourses = category.courses;
+    return concentrationInfo;
   }
 
   // The "Arts & Scripture Engagement" concentration needs to show the courses available in the category named "Choose One of the Following"
-  function getCoursesForChooseOne() {
+  function getCoursesForChooseOne(concentrationInfo) {
     $scope.chooseOne = $scope.planInfo.plan.available_courses.find(function(category) {
       return category.name == "Choose one of the following";
     });
+    return concentrationInfo;
+  }
+
+  // If a course's name is long enough that it wraps to two lines, make the date lines in the next cell line up
+  function alignDottedLines(concentrationInfo) {
+    $('.course-names').each(function(index, cell) {
+      var sourceHeights = $(cell).find('.dotted-line-container .dotted-line-left').map(function(index, element) { 
+        return $(element).outerHeight();
+      }).get();
+      var targets = $(cell).next('.course-dates').find('.dotted-line-container');
+      if (sourceHeights.length == targets.length) {
+        for (var index = 0; index < sourceHeights.length; index++) {
+          $(targets[index]).css('height', sourceHeights[index]);
+          // console.log('Adjusted', targets[index], 'to', sourceHeights[index])
+        }
+      } else {
+        console.warn('Mismatch in number of courses for', cell, sourceHeights, targets);
+      }
+    });
+    $('.cell-header').each(function(index, element) {
+      var sourceHeight = $(element).outerHeight();
+      var $target = $(element).parent().next().find('.dotted-line-spacer');
+      $target.css('height', sourceHeight);
+    });
+    return concentrationInfo;
   }
 
   $scope.totalCoreCredits = countCredits(plan.plan.required_courses);
-  $scope.$watch('planInfo.plan.concentration', getCategoryHourTotals);
-  $scope.$watch('planInfo.plan.concentration', getConcentrationSpecificCourses);
-  $scope.$watch('planInfo.plan.concentration', getCoursesForChooseOne);
-
+  $scope.$watch('planInfo.plan.concentration', getConcentrationInfo);
 
 }]);
